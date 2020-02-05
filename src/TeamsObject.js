e59fb4d4277c350ba3b17133f8023664d7b2383a
Darya <d.tsvetkova@storytelling-software.com>
2020-02-05 13:22:05 +0300

    The data about the teams is moved to a separate file

---
+++ src/TeamsObject.js
@@ -0,0 +1,41 @@
+let teams = {
+    TL: {
+        name: 'Team Liquid',
+        logo_url: process.env.PUBLIC_URL + 'img/teams/logos/Team_Liquid.png',
+        players: [
+            {
+                name: 'm`iCKe',
+                position: '1',
+                flag_url: 'img/teams/flags/Sweden.png'
+            }, {
+                name: 'qojqva',
+                position: '2',
+                flag_url: 'img/teams/flags/Germany.png'
+            }, {
+                name: 'Boxi',
+                position: '3',
+                flag_url: 'uimg/teams/flags/Sweden.png'
+            }, {
+                name: 'Taiga',
+                position: '4',
+                flag_url: 'img/teams/flags/Norway.png'
+            }, {
+                name: 'PiNSaNiA',
+                position: '5',
+                flag_url: 'img/teams/flags/Sweden.png'
+            }, {
+                name: 'Blitz',
+                position: 'c',
+                flag_url: 'img/teams/flags/United_States.png'
+            },
+        ]
+    },
+    TS: {
+        name: 'Team Secret',
+        logo_url: process.env.PUBLIC_URL +  'img/teams/logos/Team_Secret.png',
+    },
+
+
+}
+
+export default teams;
